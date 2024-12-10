2f19ebdd1adc8800071e2c9e72def7d2261de85a
PrgAhmad <ahmadrazaansari340@gmail.com>
2024-12-10 10:17:19 +0530

    added

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,16 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCountryByName } from "../api/countryHomeApi";
 
 export const CountryDetails = () => {
-    const { name } = useParams();
-    useEffect(async() => {
-        const res = await getCountryByName(name);
-        const data = res[0];
-        console.log(data);
-        
-    })
-    return (<section className="h-screen">
-        <div>Hello</div>
-    </section>);
-}
+  const { name } = useParams();
+  const [country, setCountry] = useState(null);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await getCountryByName(name);
+      const data = res[0];
+      setCountry(data);
+    };
+    getData();
+  }, []);
+  if (country !== null) {
+    console.log(country.flags.png);
+  }
+
+  return (
+    <section className="h-screen grid place-items-center">
+      <div><img src={country?.flags.png} alt="" /></div>
+    </section>
+  );
+};
